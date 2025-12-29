8e77a50aab424924a4d8e77e5085e202192c28c7
Steve Sutton <stevennsutton@gmail.com>
2025-12-29 00:51:02 -0500

    Clean up repo

--- update-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Add totalRounds and isCompleted to mock League objects in tests
-find src/__tests__ -name "*.test.ts" -type f -exec sed -i 's/participants: \[\([^]]*\)\],$/participants: [\1],\n      totalRounds: 10,\n      isCompleted: false,/g' {} \;
-# Add notificationsSent to mock Round objects
-find src/__tests__ -name "*.test.ts" -type f -exec sed -i 's/votes: \[\([^]]*\)\],$/votes: [\1],\n        notificationsSent: {\n          roundStarted: false,\n          submissionReminder: false,\n          votingStarted: false,\n          votingReminder: false,\n          allVotesReceived: false\n        },/g' {} \;
